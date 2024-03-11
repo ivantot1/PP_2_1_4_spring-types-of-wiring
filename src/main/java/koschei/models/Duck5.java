2b8f75ed24bb30c2f9168bb14cb7d9d6aa64da39
Ivan <totmeshkin@gmail.com>
2024-03-11 20:32:36 +0300

    Fix

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -5,13 +5,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
-
-
-    @Autowired
     private Egg6 egg6;
 
     @Override
     public String toString() {
         return ", в утке яйцо " + egg6.toString();
+    }
+
+    @Autowired
+    public void setEgg6(Egg6 egg6) {
+        this.egg6 = egg6;
     }
 }
--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -7,8 +7,11 @@ import org.springframework.stereotype.Component;
 public class Egg6 {
 
 
-    @Autowired
-    private Needle7 needle7;
+    private final Needle7 needle7;
+
+    public Egg6(Needle7 needle7) {
+        this.needle7 = needle7;
+    }
 
     @Override
     public String toString() {
